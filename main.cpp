a88a8209830e109f8be6a036efd8ff6d7da70ab8
onorio <onoriofelipe7@gmail.com>
2023-12-14 22:27:24 -0300

    Link boost and update notes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <boost/signals2.hpp>
 
 int main(){
+    boost::signals2::signal<void (int a)> emitter;
+    auto lambda = [](int x){ std::cout << x << std::endl; };
+    emitter.connect(lambda);
     std::cout << "aaaaaaaaaaaaaaaaaa" << std::endl;
+    emitter(23);
+    emitter(14);
 }
